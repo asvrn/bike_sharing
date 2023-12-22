78bed6be6f6ff9fc39d100665e7ed8f3aee46396
asvrn <rifqiasverian16@gmail.com>
2023-12-22 14:51:22 +0700

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -147,7 +147,7 @@ weather_rent_df = weather_rent_df.sort_values(by='count', ascending=False)
 sns.barplot(
     x=weather_rent_df.index,
     y=weather_rent_df['count'],
-    hue="color", legend=False,
+    hue=weather_rent_df.index, palette="bright", legend=False,
     # ax=ax,
     width=0.5
 )
